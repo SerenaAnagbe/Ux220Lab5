ac89647afcc2d545dd6504a74a8ec7b400c4ca56
serenaanagbe <anag0050@mylaurier.ca>
2023-11-08 21:18:40 -0500

    add new for lab 5

--- customelements.js
+++ customelements.js
@@ -5,3 +5,39 @@ class SerenaElement extends HTMLElement{
 }
 
 customElements.define("x-serena", SerenaElement);
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>`;
+    
+    }
+}
+
+customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
+
+class RightElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:right">${this.innerHTML}</div>`;
+    
+    }
+}
+
+customElements.define("x-right", RightElement);
+
+class BlueElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="color:blue">${this.innerHTML}</div>`;
+    
+    }
+}
+
+customElements.define("x-blue", BlueElement);
